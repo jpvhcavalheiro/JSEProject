70313f579f899d4371582b10d71af46a8f8472a8
jpvhcavalheiro <jpvhcavalheiro@gmail.com>
2018-11-19 20:10:18 +0000

    update

--- StockManagement/src/repositories/ProductRepository.java
+++ StockManagement/src/repositories/ProductRepository.java
@@ -1,6 +1,13 @@
 package repositories;
+
 import io.altar.jseproject.model.Product;
 
-public class ProductRepository extends EntityRepository <Product> {
+public class ProductRepository extends EntityRepository<Product> {
+
+	private final static ProductRepository INSTANCE = new ProductRepository();
+
+	public static ProductRepository getInstance() {
+		return INSTANCE;
+	}
 
 }
